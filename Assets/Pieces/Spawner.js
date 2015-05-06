d80a7f1db33542c3a10c8a048d47f2beeb66efaa
Nikolay Astakhov <nikolay.astakhov@gmail.com>
2015-05-06 19:04:25 +0300

    Commiting things already done

---
+++ Assets/Pieces/Spawner.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var groups : GameObject[];
+
+function Start () {
+  spawnNext();
+}
+
+function spawnNext () {
+  var i : int = Random.Range(0, groups.Length);
+  Instantiate(groups[i], transform.position, Quaternion.identity);
+}
