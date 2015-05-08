9a5513a7b97e028b4668761d9c672b6d2fba0589
MarchenkoSI <marchenko.s.i@gmail.com>
2015-05-08 23:53:40 +0400

    Добавлена основа для подсчета очков. Слегка переработано меню паузы

---
+++ Assets/Scripts/Score.js
@@ -0,0 +1,15 @@
+﻿static var score : int;        // The player's score.
+private var text : Text;       // Reference to the Text component.
+
+
+function Awake ()
+{
+    text = GetComponent (Text);
+    score = 0;
+}
+
+
+function Update ()
+{
+    text.text = " " + score;
+}
