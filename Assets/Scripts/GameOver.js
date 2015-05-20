a49d256b8a35b715a4648d23294540c4628fe57f
MarchenkoSI <marchenko.s.i@gmail.com>
2015-05-20 07:49:23 +0400

    Добавлено главное меню

---
+++ Assets/Scripts/GameOver.js
@@ -0,0 +1,15 @@
+#pragma strict
+import UnityEngine.UI;
+public var GameOverCan : Canvas;
+public var MainMenu : Button;
+
+function Start () {
+	GameOverCan = GameOverCan.GetComponent.<Canvas>();
+    MainMenu = GetComponent.<Button> ();
+}
+
+
+
+public function StartLevel (){
+        Application.LoadLevel (0); //this will load our first level from our build settings. "1" is the second scene in our game
+}
---
+++ Assets/Scripts/MainMenu.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+import UnityEngine.UI;
+public var StartMenu : Canvas;
+public var QuitMenu : Canvas;
+public var PauseMenu : Canvas;
+public var StartB : Button;
+public var CloseB : Button;
+
+function Start () {
+	QuitMenu = QuitMenu.GetComponent.<Canvas>();
+    StartB = GetComponent.<Button> ();
+    CloseB = GetComponent.<Button> ();
+    StartMenu.enabled = true;
+    QuitMenu.enabled = false;
+}
+function ExitPress (){
+    StartMenu.enabled = false;
+    QuitMenu.enabled = true;
+}
+
+function NoPress (){
+    QuitMenu.enabled = false;
+    StartMenu.enabled = true;
+}
+
+public function ExitGame (){
+    Application.Quit(); 
+}
+
+public function StartLevel (){
+        Application.LoadLevel (1); //this will load our first level from our build settings. "1" is the second scene in our game
+}
