0232c582d39432239036f3ecc355eefcc1a41309
Nikolay Astakhov <nikolay.astakhov@gmail.com>
2015-05-29 22:23:01 +0300

    Сделан звук при перемещении, отключение звука

---
+++ Assets/Scripts/Sound.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+static var muted : boolean = false;
+var moveClip : AudioClip;
+
+function Update () {
+  if (Input.GetKeyDown(KeyCode.M)) {
+    mute();
+  }
+
+  if (Input.GetKeyDown(KeyCode.W) ||
+      Input.GetKeyDown(KeyCode.S) ||
+      Input.GetKeyDown(KeyCode.A) ||
+      Input.GetKeyDown(KeyCode.D) ||
+      Input.GetKeyDown(KeyCode.UpArrow) ||
+      Input.GetKeyDown(KeyCode.LeftArrow) ||
+      Input.GetKeyDown(KeyCode.DownArrow) ||
+      Input.GetKeyDown(KeyCode.RightArrow) ||
+      Input.GetKeyDown(KeyCode.Space)) {
+    GetComponent.<AudioSource>().PlayOneShot(moveClip);
+  }
+}
+
+function mute () {
+  if (!muted) {
+    AudioListener.pause = true;
+    muted = true;
+  } else {
+    AudioListener.pause = false;
+    muted = false;
+  }
+}
